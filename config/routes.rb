b10ad7c0fdffc39c4d6f4cafee4b885c34aa0fde
wuwica <wuwicajon@gmail.com>
2018-06-20 17:49:17 -0400

    added plain pages for the other tabs

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'accessed/index' => 'accessed#index'
   get 'accessed/deposit' => 'accessed#deposit'
+  get 'accessed/statistics' => 'accessed#statistics'
+  get 'accessed/transactions' => 'accessed#transactions'
+ 
   root "home#index"
   get 'callback' => 'callback#gettoken'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
